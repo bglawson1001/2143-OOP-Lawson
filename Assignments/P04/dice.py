8e12478fa9717f1b6d36a22b7ad3096f82a4d9e3
bglawson1001 <122930732+bglawson1001@users.noreply.github.com>
2023-04-25 11:46:33 -0500

    Update dice.py

--- Assignments/P04/dice.py
+++ Assignments/P04/dice.py
@@ -1,8 +1,7 @@
 # Brayden Lawson
 # P04
 # April 24, 2023
-# This program is a conversion of a header file and main.cpp file from C++ it uses a dice class 
-# which will be used in the D&D program. 
+# This program is a conversion of a header file and main.cpp file from C++ it uses a dice class which will be used in the next program
 
 import sys
 from rich import print
@@ -11,21 +10,27 @@ import random
 
 
 class Die(object):
-  def __init__(self,sides=None,):
-    if not sides:
-      self.sides = 6
+
+  
+
+    
+  
+  def __init__(self,sides = 0):
+    if str(sides).isdigit():
+      self.sides = sides
     else:
       if not isinstance(sides, int):
         print("error: sides is not an integer!")
-        sys.exit()
-      self.sides = sides
+        print("Default value used")
+      
+      self.sides = 6
 
   def roll(self):
     values = [x for x in range(self.sides)]
     
     random.shuffle(values)
-    
-    return values[0] + 1
+    if values:
+      return values[0] + 1
     
   def __str__(self):
     return f"[sides: {self.sides}]"
@@ -33,33 +38,36 @@ class Die(object):
 
 class Dice:
   def __init__(self,sides=None,num_dice=1):
+    parts=[]
+    sides2 = [sides]
+    
+    self.sides = sides
+      
     if not sides:
       print("Need to pass in sides!!!")
       sys.exit()
-
-    # elif "." in sides:
-    #   # 8.d.10
-    #   parts = sides.split(".")
-    #   self.sides = parts[0]
-    #   self.num_dice = parts[1]
-    #   self.sides = int(self.num_dice)
-    # else: 
-    #   self.sides = sides
-    #   self.num_dice = num_dice
-
+    
+    elif isinstance(sides,str):
+      # 8.d.10
+      parts = sides.split(".")
+      # self.sides = parts[0]
+      num_dice = int(parts[0])
+      self.sides = int(parts[2])
+      # print(sides, self.sides,self.num_dice)
+    else: 
+      self.sides = sides
+      self.num_dice = num_dice
+      
       def __str__(self):
         return f"{self.sides} , {self.num_dice}"
       #x = sides.split(".")
      #numberofdice, sides = int(x[0]), int(x[2])
       
-
-    self.sides = sides
-      
     # create a container
     self.dice = []
     
     for die in range(num_dice):
-      self.dice.append(Die(sides))
+      self.dice.append(Die(self.sides))
 
   def sum(self):
     total = 0
@@ -72,7 +80,7 @@ class Dice:
     results = []
     for d in self.dice:
       results.append(d.roll())
-
+    # print(results)
     return max(results)
 ## method to get the min of the amount of rolls passed from whatever sided die is passed into main.
   def min(self):
@@ -134,7 +142,5 @@ if __name__=='__main__':
 
   print(s1)
   print(s2)
-  # for i in range(100):
-  #   print(d3.roll())
-  
+
   
