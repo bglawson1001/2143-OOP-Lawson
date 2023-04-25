670b59a60dc5f453304b3f4b49f16926fcd0eca5
bglawson1001 <122930732+bglawson1001@users.noreply.github.com>
2023-04-25 11:44:27 -0500

    Update main.py

--- Assignments/P04/main.py
+++ Assignments/P04/main.py
@@ -4,7 +4,7 @@ from dice import Dice
 def dieTester(die, runs=10, testType="sum"):
     """Example function to test a die or dice.
     """
-    #print(type(die))
+    
     if isinstance(die, Die):
         print(f"Testing {die.sides} sided die for {runs} rolls:")
         print("    [ ", end="")
@@ -12,9 +12,9 @@ def dieTester(die, runs=10, testType="sum"):
             print(die.roll(), end=" ")
         print("]")
     else:
-        #print("I'm a dice....")
+        
         print(f"Rolling {len(die.dice)} {die.sides} sided die {runs} times to get the {testType} value:")
-        #print(f"Rolling {len(die.dice)}")
+        
 
         print("    [ ", end="")
         for i in range(runs):
@@ -34,14 +34,14 @@ if __name__ == '__main__':
   print("Brayden Lawson")
   print("4-24-23")
   print("Spring 2143")
-  d1 = Die()
+  d1 = Die(6)
   d2 = Die(20)
   d3 = Dice(10, 5)
-  #d4 = Dice("8.d.20")
+  d4 = Dice("8.d.20")
 
   dieTester(d1, 10)
   dieTester(d2, 20)
   dieTester(d3, 10, "max")
   dieTester(d3, 10, "min")
   dieTester(d3, 10, "avg")
-  #dieTester(d4, 20, "max")
+  dieTester(d4, 20, "max")
